06aad0613b0ebdd6db8762dcbc60ef3195996aa7
JosR <josueramos17@hotmail.com>
2023-05-03 14:22:34 -0500

    feat: Added LogIn.vue

---
+++ src/views/LogIn.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <pv-button class="translate-x-200" type="submit" label="Register"></pv-button>
+    </header>
+    <div class=" flex  align-items-center justify-content-center align-content-center flex-wrap">
+        <pv-card>
+          <template #title> Log In </template>
+          <template #content>
+            <p class="mb-1">Username</p>
+              <pv-input-text type="text" v-model="value"></pv-input-text>
+              <p class="mb-1">Password</p>
+              <pv-password v-model="value" toggle-mask :feedback="false"></pv-password>
+              <p></p>
+              <pv-button type="submit" label="Submit"></pv-button>
+          </template>
+            <template #footer>Don't have an account? <router-link to="/Register">Register</router-link></template>
+    </pv-card>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
