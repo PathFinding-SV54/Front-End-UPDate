0b75d5e3d3e98a257b1958e40cfaea1f2079cfd8
Aldo Medina <u202018286@upc.edu.pe>
2023-05-03 18:21:18 -0500

    (fix) Routing and base path fixed.

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LogIn from "@/views/LogIn.vue";
+
+const routes = [
+    { path: '/', redirect: "/login" },
+    { path: '/login', component: LogIn },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path:'/log-in',
-      name: 'login',
-      component:()=> import('../views/LogIn.vue')
-    }
-  ]
+    history: createWebHistory(),
+    routes,
 })
 
 export default router
