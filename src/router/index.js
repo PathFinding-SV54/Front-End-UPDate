1af89c1ec98576f8aedd36b49d9a363c189805aa
Aldo Medina <u202018286@upc.edu.pe>
2023-06-07 04:10:53 -0500

    Refactor: Log In component has been refactored.

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LogIn from "@/components/views/LogIn.vue";
 import NewCommunity from "@/components/new-community.vue";
 import Suscription from "@/components/suscription.component.vue";
 import NewActivity from "@/components/new-activity.vue";
+import LoginComponent from "@/components/views/pages/login.component.vue";
 
 const routes = [
     { path: '/', redirect: "/login" },
-    { path: '/login', component: LogIn },
+    { path: '/login', component: LoginComponent},
     { path: '/new-activity', component: NewActivity},
     { path: '/new-community', component: NewCommunity },
     { path: '/suscription', component: Suscription },
