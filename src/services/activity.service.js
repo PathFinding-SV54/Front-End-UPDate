47a83176ef5c58c0817cc5070302411ae6444970
Aldo Medina <u202018286@upc.edu.pe>
2023-06-26 15:05:46 -0500

    Feat: added services and deployed api environment.

--- src/services/activity.service.js
+++ src/services/activity.service.js
@@ -1,8 +1,4 @@
-import axios from "axios";
-
-const http = axios.create({
-    baseURL: "https://localhost:7029/api",
-});
+import http from "./http-common";
 
 export class ActivityService {
     getActivities = () => {
